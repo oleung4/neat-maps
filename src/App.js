16807b87453299c99de433a9e3e41543f329819d
Oliver Leung <oliver.hon.pan.leung@gmail.com>
2019-05-23 22:20:10 +0800

    added basic layout + csv parser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CsvParse from "@vtex/react-csv-parse";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    csvRaw: null
+  };
+
+  handleData = data => {
+    // console.log(data);
+    this.setState({ csvRaw: data });
+    console.log(this.state.csvRaw[0]);
+  };
+
+  render() {
+    const keys = ["CATEGORY", "STATE", "CITY", "ZIPCODE", "ADDRESS"];
+
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <h1>upload csv here</h1>
+        </div>
+        <div className="row justify-content-center">
+          <CsvParse
+            keys={keys}
+            onDataUploaded={this.handleData}
+            onError={this.handleError}
+            render={onChange => (
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">Upload</span>
+                </div>
+                <div className="custom-file">
+                  <input
+                    type="file"
+                    accept=".csv"
+                    className="custom-file-input"
+                    id="inputGroupFile01"
+                    onChange={onChange}
+                  />
+                  <label
+                    className="custom-file-label"
+                    htmlFor="inputGroupFile01"
+                  >
+                    Choose file
+                  </label>
+                </div>
+              </div>
+            )}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
